99d0ab596634a4a6edf8f0428adfe77f76602fdb
David Sabau <127231858+davosab@users.noreply.github.com>
2025-03-06 16:58:35 +0200

    problem 1 & 2 done

--- Curs1/src/P2.java
+++ Curs1/src/P2.java
@@ -1,9 +1,25 @@
+/*
+Sa se citeasca de la tastatura un numar. Daca este cuprins intre 1 si 5 sa 
+se afiseze numarul respectiv dar in litere. Daca este in afara intervalului 
+sa se afiseze INVALID.
+ */
+
+import java.util.*;
 
 public class P2 {
 
-public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	System.out.println("THIS WORKS TOO");
+  public static void main(String[] args) {
+
+	List<String> nrs = List.of("UNU", "DOI", "TREI", "PATRU", "CINCI");
+	Scanner myScanner = new Scanner(System.in);
+	int userNr = myScanner.nextInt();
+	
+	if (userNr < 1 || userNr > 5) {
+		System.out.println("INVALID");
+		return;
+	}
+	
+	System.out.println(nrs.get(userNr - 1));
   }
 
 }
