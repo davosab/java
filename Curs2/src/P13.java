7abfd303f4a2aee3b129a69c69e0a5ebb06c3034
David Sabau <127231858+davosab@users.noreply.github.com>
2025-03-07 19:14:24 +0200

    problems 1 - 16

--- Curs2/src/P13.java
+++ Curs2/src/P13.java
@@ -6,9 +6,12 @@ divizibil cu 17 iar al doilea cu 19.
 public class P13 {
 
   public static void main(String[] args) {
-	
-  
 
+    for (int n17 = 17; n17 < 1000; n17 += 17) {
+      if ((1000 - n17) % 19 == 0) {
+        System.out.println(n17 + " " + (1000 - n17));
+      }
+    }
   }
 
 }
