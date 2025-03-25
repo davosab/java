180cb06cee030329d17fe374aec082069c70d3d6
David Sabau <127231858+davosab@users.noreply.github.com>
2025-03-25 09:27:56 +0200

    Update Patrat.java

--- curs4/src/Patrat.java
+++ curs4/src/Patrat.java
@@ -12,15 +12,15 @@ public class Patrat {
     this.a = this.l * this.l;
   }
 
-  public void potrivita() {
-    System.out.println("Patrat " + this.l + " Aria " + this.a);
+  public String toString() {
+    return "Patrat " + this.l + " Aria " + this.a;
   }
 
   public static void main(String[] args) {
     Patrat p1 = new Patrat();
     Patrat p2 = new Patrat(9);
 
-    p1.potrivita();
-    p2.potrivita();
+    System.out.println(p1);
+    System.out.println(p2);
   }
 }
