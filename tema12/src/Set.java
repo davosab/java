2be700984d9bbac10154ae7ea5c6a107af5e31c2
David Sabau <127231858+davosab@users.noreply.github.com>
2025-04-17 20:16:39 +0300

    Update Set.java

--- tema12/src/Set.java
+++ tema12/src/Set.java
@@ -1,1 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashSet;
 
+public class Set {
+  public static void main(String[] args) {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+      BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+      String line;
+      int t = Integer.parseInt(br.readLine()); // just to eliminate t from pairs
+      HashSet<String> uniquePairs = new HashSet<String>();
+
+      while ((line = br.readLine()) != null) {
+        uniquePairs.add(line);
+        bw.write(uniquePairs.size() + "\n");
+        System.out.println(uniquePairs.size());
+      }
+      br.close();
+      bw.close();
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
