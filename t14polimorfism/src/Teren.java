58995903e9dacc6cc65c20df65e6cffd3bfac688
David Sabau <127231858+davosab@users.noreply.github.com>
2025-04-30 17:48:59 +0300

    Update Teren.java

--- t14polimorfism/src/Teren.java
+++ t14polimorfism/src/Teren.java
@@ -1,14 +1,15 @@
 public class Teren extends Proprietate {
 
-  private int rang;
+  private Rang rang;
   
   public Teren(String tip, String strada, int nr, int suprafata, int rang) {
     super(strada, nr, suprafata);
+    this.rang = Rang.fromCod(rang);
     this.setTip(tip);
     this.setCost(350 * suprafata / rang);
   }
 
   public int getRang() {
-    return rang;
+    return rang.getCod();
   }
 }
